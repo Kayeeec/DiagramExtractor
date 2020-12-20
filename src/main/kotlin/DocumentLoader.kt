6e4080544e170943f249de12df05012df3db7495
Karolína Božková <karolina.bozkova@gmail.com>
2020-12-20 20:30:21 +0100

    squashed - diagram extraction

--- src/main/kotlin/DocumentLoader.kt
+++
@@ -1,9 +0,0 @@
-import org.apache.pdfbox.pdmodel.PDDocument
-import java.io.File
-
-class DocumentLoader(val fileName: String = "testPage.pdf") {
-    var document: PDDocument
-    init {
-        this.document = PDDocument.load(File(javaClass.getResource(this.fileName).toURI()))
-    }
-}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,18 @@
-import org.apache.pdfbox.pdmodel.PDDocument
-import org.apache.pdfbox.text.PDFTextStripper
-import org.apache.pdfbox.text.TextPosition
 import java.io.File
-import java.io.IOException
-import java.util.*
-
 
 fun main() {
     println("Extractor running...")
-    val document = DocumentLoader().document
-    document.use { document ->
-        val wordSearcher = WordSearcher()
-        val findSubwords = wordSearcher.findSubwords(document, 1, listOf("Diagram", "Summary"))
-        println(findSubwords)
+
+    val testFilesFolderPath = "test_files/"
+    val testFiles = File(testFilesFolderPath).list()
+    testFiles?.forEach {
+        val filePath = "${testFilesFolderPath}${it}"
+        print("   Extracting diagrams from $filePath...")
+        val extractor = DiagramExtractor(filePath)
+        extractor.extractDiagrams()
+        println(" done.")
     }
+
     println("Extractor finished.")
 }
-
 
