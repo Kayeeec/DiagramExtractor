7a4ce30e369130e83e496c94f8042de730f8a195
Karolína Božková <karolina.bozkova@gmail.com>
2020-12-20 15:15:51 +0100

    wip

--- src/main/kotlin/DiagramExtractor.kt
+++ src/main/kotlin/DiagramExtractor.kt
@@ -1,3 +1,6 @@
+import org.apache.batik.anim.dom.SVGDOMImplementation
+import org.apache.batik.svggen.SVGGeneratorContext
+import org.apache.batik.svggen.SVGGraphics2D
 import org.apache.pdfbox.contentstream.operator.Operator
 import org.apache.pdfbox.cos.COSDocument
 import org.apache.pdfbox.cos.COSInteger
@@ -7,22 +10,35 @@ import org.apache.pdfbox.pdmodel.PDDocument
 import org.apache.pdfbox.pdmodel.PDPage
 import org.apache.pdfbox.pdmodel.common.PDRectangle
 import org.apache.pdfbox.pdmodel.common.PDStream
+import org.apache.pdfbox.rendering.PDFRenderer
 import types.SearchedPage
+import java.awt.Color
 import java.io.File
+import kotlin.math.max
 
-class DiagramExtractor(val fileName: String = "testPage.pdf") {
-    val diagramPages = mutableListOf<DiagramPageResult>()
+class DiagramExtractor(val filePath: String = "testPage.pdf") {
+    private val diagramPages = mutableListOf<DiagramPageResult>()
+    private var maxDiagramPageNumber = 0
+    private lateinit var fileName: String
+    private lateinit var document: PDDocument
+
+    private val paddingLength: Int
+        get() = "${this.maxDiagramPageNumber}".length
 
     companion object {
         const val TEMPLATE_FILE = "template.pdf"
     }
 
+    init {
+        this.loadDocumentAndSetFileName()
+    }
+
     fun extractDiagrams() {
-        val document = this.loadDocument()
-        document.use { pdDocument: PDDocument? ->
+        this.document.use { pdDocument: PDDocument? ->
             this.collectDiagramPages(document)
             for (dpage: DiagramPageResult in this.diagramPages) {
-                this.extractDiagram(dpage)
+                this.maxDiagramPageNumber = max(maxDiagramPageNumber, dpage.pageNumber)
+                this.extractDiagramFromPage(dpage)
             }
         }
     }
@@ -67,9 +83,9 @@ class DiagramExtractor(val fileName: String = "testPage.pdf") {
      * then buffer diagram operators until next BT
      * create pdf page with the diagram operators
      * crop it
-     * convert it to svg/png //todo
+     * convert it to svg
      */
-    private fun extractDiagram(dpage: DiagramPageResult) {
+    private fun extractDiagramFromPage(dpage: DiagramPageResult) {
         // collect diagram tokens
         val pdfStreamParser = PDFStreamParser(dpage.pdPage)
         var token = pdfStreamParser.parseNextToken()
@@ -96,17 +112,22 @@ class DiagramExtractor(val fileName: String = "testPage.pdf") {
             val page = templateDoc.getPage(0)
             cropPage(page, dpage.searchedPage.diagramHeight)
             page.setContents(pdStream)
-
-            // for now just save into a new doc and check //todo remove
-            templateDoc.save("${this.fileName}_page_${dpage.pageNumber}.pdf")
-
-
-
-            // crop page
-            // TODO: 19.12.20
-            //convert page into svg/png
-            // TODO: 19.12.20
+            convertPageToSvg(templateDoc, dpage.pageNumber)
         }
+    }
+
+    private fun convertPageToSvg(tempDoc: PDDocument, pageNumber: Int) {
+        val namespace = SVGDOMImplementation.SVG_NAMESPACE_URI
+        val impl = SVGDOMImplementation.getDOMImplementation()
+        val doc = impl.createDocument(namespace, "svg", null)
+        val ctx = SVGGeneratorContext.createDefault(doc)
+        ctx.isEmbeddedFontsOn = true
+        val svgFilename = "${this.fileName}_${pageNumber.toString().padStart(this.paddingLength, '0')}.svg"
+        val svgGraphics2D = SVGGraphics2D(ctx, false)
+        val pdfRenderer = PDFRenderer(tempDoc)
+        svgGraphics2D.background = Color(255, 255, 255)
+        pdfRenderer.renderPageToGraphics(0, svgGraphics2D)
+        svgGraphics2D.stream(svgFilename)
     }
 
     private fun cropPage(page: PDPage, diagramHeight: Float?) {
@@ -119,8 +140,10 @@ class DiagramExtractor(val fileName: String = "testPage.pdf") {
         page.cropBox = pdRectangle
     }
 
-    private fun loadDocument(): PDDocument {
-        return PDDocument.load(File(javaClass.getResource(this.fileName).toURI())) // TODO: 17.12.20 handle IO errors
+    private fun loadDocumentAndSetFileName() {
+        val file = File(javaClass.getResource(this.filePath).toURI())
+        this.document = PDDocument.load(file) // TODO: 17.12.20 handle IO errors
+        this.fileName = file.name
     }
 
     private fun loadTemplateDocument(): COSDocument {
@@ -140,7 +163,7 @@ class DiagramExtractor(val fileName: String = "testPage.pdf") {
 }
 
 data class DiagramPageResult(
-    val pageNumber: Int, // 1-based, // todo needed?
+    val pageNumber: Int, // 1-based
     val pdPage: PDPage,
     val searchedPage: SearchedPage,
 )
